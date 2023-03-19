fb02d05d54395a4993c8c09c8ad4dbf0541bb19a
Aviral Singh <aviralsingh2714@gmail.com>
2023-03-20 02:27:53 +0530

    Designing the welcome page and adding custom fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,30 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react"; 
+import { useFonts } from "expo-font"; //to use custom fonts
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack/>;
+    //loading font into a hook
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+    })
+
+    //async callback function is used, we only show homepage when our fonts are loaded 
+    const onLayoutRootView = useCallback(async () => {
+        if(fontsLoaded){
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]) //dependancy array
+
+    //no fonts loaded we return null
+    if(!fontsLoaded) return null;
+
+    //ensured that the font bee loaded
+    return <Stack onLayout = {onLayoutRootView} />;
 }
 
 export default Layout;
