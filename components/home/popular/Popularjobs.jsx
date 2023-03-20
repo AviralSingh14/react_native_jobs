a2fba083e2af45874a2c83e176771210c88321ed
Aviral Singh <aviralsingh2714@gmail.com>
2023-03-20 19:23:39 +0530

    added a few tweaks in search etc, created the company overview and apply page

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -17,10 +17,9 @@ const Popularjobs = () => {
   const [selectedJob, setSelectedJob] = useState()
 
   const handleCardPress = (item) => {
-
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id)
   }
-
-  // console.log(data)
 
   return (
     <View style={styles.container}>
